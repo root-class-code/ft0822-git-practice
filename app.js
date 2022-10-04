feb16c24f92c0db132b3c9dd10df03e8cb902a2e
ManishPoduval <manishpoduval@hotmail.com>
2022-10-04 12:10:21 +0530

    changed text

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ require("./config")(app);
 const capitalized = require("./utils/capitalized");
 const projectName = "tweettier";
 
-app.locals.appTitle = `${capitalized(projectName)} created with RootLauncher`;
+app.locals.appTitle = `${capitalized(projectName)} created with RootLauncher v2`;
 
 // 👇 Start handling routes here
 const index = require("./routes/index.routes");
